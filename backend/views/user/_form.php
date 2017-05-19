f0ba4e31c1930dfcdffeb549a256ee5e1ea77609
Feiou Zhang <feiou.zhang@hotmail.com>
2017-05-18 19:59:14 -0500

    change to drop down about the user status

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -12,7 +12,10 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->dropDownList(\common\models\User::allStatus(),
+        ['prompt'=>'Select Status']); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
