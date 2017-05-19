2ba08c27211e0ba1a773961b7b3c518f176cbc5c
Feiou Zhang <feiou.zhang@hotmail.com>
2017-05-18 21:23:05 -0500

    *need to fix the display text
    refer to issue 2

--- backend/views/comment/update.php
+++ backend/views/comment/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Comment */
 
-$this->title = 'Update Comment: ' . $model->id;
+$this->title = 'Update Comment: ' . $model->post_id; //can change to the post title
 $this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
