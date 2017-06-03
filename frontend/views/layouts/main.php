373cbea06f4c8bf32a3ab2adb8290a734b4a2761
Feiou Zhang <feiou.zhang@hotmail.com>
2017-06-02 23:33:45 -0500

    modify the nav manu

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,8 +36,9 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Blog', 'url' => ['/post/index']],
+        ['label' => 'Resume', 'url' => ['/site/about']],
+//        ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
